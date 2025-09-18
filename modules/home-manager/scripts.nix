7636ac0b06d3250b21bfa1d489d252020cfb920c
Sammy Al Hashemi <sammy@salh.xyz>
2025-09-17 20:36:41 -0400

    `let` in bash is for arithmetic expr apparently.

--- modules/home-manager/scripts.nix
+++ modules/home-manager/scripts.nix
@@ -9,7 +9,7 @@ let
   BOOKMARKS = "~/.bookmarks";
 in
 pkgs.writeShellScriptBin "wofi-bookmark" ''
-  let chosen=`cat ${BOOKMARKS} | wofi --show=dmenu | awk '{ print $2 }'`
+  chosen=`cat ${BOOKMARKS} | wofi --show=dmenu | awk '{ print $2 }'`
 
   if [[ ! $chosen ]]; then
     return
