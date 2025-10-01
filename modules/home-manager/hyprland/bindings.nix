90e51df97dfa8568ae761e00822cf5f4cdb12ad5
Sammy Al Hashemi <sammy@salh.xyz>
2025-09-30 22:26:31 -0400

    undoing tragic mistake.

--- modules/home-manager/hyprland/bindings.nix
+++ modules/home-manager/hyprland/bindings.nix
@@ -80,6 +80,35 @@ in
       "SUPER SHIFT, 9, movetoworkspace, 9"
       "SUPER SHIFT, 0, movetoworkspace, 10"
 
+      # Swap active window with the one next to it with mainMod + SHIFT + arrow keys
+      "SUPER SHIFT, left, swapwindow, l"
+      "SUPER SHIFT, right, swapwindow, r"
+      "SUPER SHIFT, up, swapwindow, u"
+      "SUPER SHIFT, down, swapwindow, d"
+      "SUPER SHIFT, L, swapwindow, l"
+      "SUPER SHIFT, H, swapwindow, r"
+      "SUPER SHIFT, K, swapwindow, u"
+      "SUPER SHIFT, J, swapwindow, d"
+
+      # Resize active window
+      "SUPER, minus, resizeactive, -100 0"
+      "SUPER, equal, resizeactive, 100 0"
+      "SUPER SHIFT, minus, resizeactive, 0 -100"
+      "SUPER SHIFT, equal, resizeactive, 0 100"
+
+      # Scroll through existing workspaces with mainMod + scroll
+      "SUPER, mouse_down, workspace, e+1"
+      "SUPER, mouse_up, workspace, e-1"
+
+      # Control Apple Display brightness
+      "CTRL, F1, exec, ~/.local/share/omarchy/bin/apple-display-brightness -5000"
+      "CTRL, F2, exec, ~/.local/share/omarchy/bin/apple-display-brightness +5000"
+      "SHIFT CTRL, F2, exec, ~/.local/share/omarchy/bin/apple-display-brightness +60000"
+
+      # Super workspace floating layer
+      "SUPER, S, togglespecialworkspace, magic"
+      "SUPER SHIFT, S, movetoworkspace, special:magic"
+
       # Screenshots
       ", PRINT, exec, hyprshot -m region"
       "SHIFT, PRINT, exec, hyprshot -m window"
