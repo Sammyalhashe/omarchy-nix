45a43fb1d047af7faf033af87d217af097a6d063
Sammy Al Hashemi <sammy@salh.xyz>
2025-10-20 21:19:46 -0400

    Attempting to add hyprscrolling plugin.

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,12 +1,20 @@
-inputs: {
+inputs:
+{
   config,
   pkgs,
   ...
-}: {
-  imports = [./hyprland/configuration.nix];
+}:
+{
+  imports = [ ./hyprland/configuration.nix ];
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    pkgs.hyprlandPlugins.hyprscrolling = {
+      settings = {
+        column_width = 0.5;
+        fullscreen_on_one_column = false;
+      };
+    };
   };
   services.hyprpolkitagent.enable = true;
 }
